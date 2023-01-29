faf4700bceb3c21001afe85b0e42f8516b9b356b
Hugo <hdegardin@gmail.com>
2023-01-29 17:16:15 +0100

    junit depedencies fixed

--- src/test/java/com/epsile/selenium_automation/YouTubeTest.java
+++ src/test/java/com/epsile/selenium_automation/YouTubeTest.java
@@ -1,16 +1,15 @@
 package com.epsile.selenium_automation;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class YouTubeTest {
     private WebDriver driver;
 
-    @Before
+    @BeforeAll
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "//usr//bin//chromedriver");
         ChromeOptions options = new ChromeOptions();
@@ -21,9 +20,10 @@ public class YouTubeTest {
     @Test
     public void testOpenRickRollVideo() {
         driver.get("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        Assertions.assertEquals("Rick Astley - Never Gonna Give You Up (Official Music Video) - YouTube", driver.getTitle());
     }
 
-    @After
+    @AfterAll
     public void tearDown() {
         driver.quit();
     }
